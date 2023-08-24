3bc2350859f5332985c69c46b413bc9da9184f8a
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-24 18:34:32 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import PetShop
+admin.site.register(PetShop)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class PetShop(models.Model):
+    'Generated Model'
+    gdjj = models.BigIntegerField()
